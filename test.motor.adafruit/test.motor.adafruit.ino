8bcaf228303484137da1542211b696214dfdd7a6
lycying <lycying@gmail.com>
2016-10-29 19:43:50 +0800

    motor

--- test.motor.adafruit/test.motor.adafruit.ino
+++ test.motor.adafruit/test.motor.adafruit.ino
@@ -3,22 +3,49 @@
 // this code is public domain, enjoy!
 
 #include <AFMotor.h>
+#include <SoftwareSerial.h>
 
 // Connect a stepper motor with 48 steps per revolution (7.5 degree)
 // to motor port #2 (M3 and M4)
-AF_Stepper motor(100, 2);
+AF_Stepper motor1(100, 2);
 AF_Stepper motor2(100, 1);
+
+#define HC_05_RX A0
+#define HC_05_TX A5
+
+SoftwareSerial BT(HC_05_RX, HC_05_TX); 
+
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
   Serial.println("Stepper test!");
+  BT.begin(9600);
 
-  motor.setSpeed(100);  // 10 rpm   
+  motor1.setSpeed(100);  // 10 rpm   
   motor2.setSpeed(100);  // 10 rpm   
 }
 
 void loop() {
-  motor.step(1, FORWARD, DOUBLE); 
-  motor2.step(1, FORWARD, DOUBLE); 
+  
+  if (BT.available()){//Resp the data
+    
+    String val = "";
+    while (BT.available() > 0)  {
+     val += char(BT.read());
+     delay(2);
+   }
+   if(val.equals("l")){      
+          motor1.step(200, FORWARD, DOUBLE); 
+   }else if(val.equals("r")){
+          motor1.step(200, BACKWARD, DOUBLE); 
+   }else if(val.equals("u")){
+          motor2.step(200, FORWARD, DOUBLE); 
+   }else if(val.equals("d")){
+          motor2.step(200, BACKWARD, DOUBLE); 
+   }
+   
+
+  }
+  
 
 }
